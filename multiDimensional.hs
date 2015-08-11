1b21a33a936dfa40f3a7711ba76ee46f5dbc4966
Jeffrey Lyman <jeffrey.lyman@appfirst.com>
2015-08-11 17:32:07 -0500

    Created Space quasi-dependent data type

--- multiDimensional.hs
+++ multiDimensional.hs
@@ -1,137 +1,63 @@
-______________________________________________________________-
--
-- A solver is a function from a list of pieces and a space to be filled
-- and returns all possible permutataions of those peices in that space.
-- 
-- definition of the peices and space share di-
-- 3DPuzzelBoard = [[['00000000']]]
-- 3DPuzzelBoard = [[['00000000']]]
-- 3DPuzzelBoard = [[['00000000']]]
-- 3DPuzzelBoard = [[['00000000']]]
-- 3DPuzzelBoard = [[['00000000']]]
--------------------------------------------------------------------------
-import Data.List 
-import Data.Maybe
-import Control.Applicative
+{-# LANGUAGE DataKinds, GADTs, TypeFamilies, TypeOperators #-}
+------------------------------------------------------------------------
+--
+-- A solver is a function from a list of pieces and a space to be filled
+-- and returns all possible permutataions of those peices in that space.
+-- 
+-- definition of the peices and space share di-
+------------------------------------------------------------------------
+type Board n = Space Char n
+type Piece n = Space Char n
+type Puzzle n = (Board n, [Piece n])
+type ThreeDSpace = Space Char ThreeD
 
-type Row = [Char]
-type Puzzle = [Row]
-type Piece =  [Row]
-type Position = (Int,Int)
-type PieceLocation = ([Position], Piece)
+data Nat = Z | S Nat
 
-main = printMatrices $ solve myPieces emptyPuzzle
+type ThreeD = S (S (S Z))
 
-solve :: [Piece] -> Puzzle -> [Puzzle]
-solve cs z = unique $ solutions (makeLocations z cs) z 
-    where unique = nub . map (minimum . spins)
+infixl 6 :+
 
-solutions :: [[PieceLocation]] -> Puzzle -> [Puzzle]
-solutions [] z     = [z]
-solutions (l:ls) z = concatMap (solutions ls) $ validPlacements z
-    where validPlacements pz = concatMap placeInLocations l
-          placeInLocations (ps, c) = mapMaybe (place z c) ps
+type family (n :: Nat) :+ (m :: Nat) :: Nat
+type instance Z        :+ m = m
+type instance (S n)    :+ m = S (n :+ m)
 
-place :: Puzzle -> Piece -> Position -> Maybe Puzzle
-place z c (x,y) = knead (knead placeOne x) y z c
-    where placeOne '_' b = Just b
-          placeOne  a '_' = Just a
-          placeOne  _  _ = Nothing
+data Vector a n where
+    Nil  :: Vector a Z
+    (:-) :: a -> Vector a n -> Vector a (S n)
 
-knead :: (a -> b -> Maybe a) -> Int -> [a] -> [b] -> Maybe [a]
-knead _ _ zs     []     = Just zs
-knead _ _ []     [x]    = Nothing
-knead f 0 (a:as) (b:bs) = (:) <$> f a b  <*> knead f 0     as bs
-knead f x (a:as) bs     = (:) <$> Just a <*> knead f (x-1) as bs
+data Space a n where
+    Atom  :: a -> Space a Z
+    Layer :: [Space a n] -> Space a (S n)
 
+main = do 
+    puzzle <- readPuzzle "board.txt" ["pieces.txt"]
+    maybe (putStrLn "Error Reading puzzle files")
+          ((mapM_ printSpace) . solve rules)
+          puzzle
 
-makeLocations :: Puzzle -> [Piece] -> [[PieceLocation]]
-makeLocations z (c:cs) = firstLocations c : locations cs
-    where locations       = map (map (positions z) . spins)
-          firstLocations  = map ((,) [(x,y) | x <- [0..l], y <-[0..x]]) . spins
-          l               = ceiling (realToFrac (length z) / 2) - 1
+readPuzzle :: FilePath -> [FilePath] -> IO (Maybe (Puzzle n))
+readPuzzle = undefined
 
-spins :: Piece -> [Piece]
-spins c = nub $ transpose' c >>= reverse' >>= rotate'
-    where transpose' a = [a, transpose a]
-          reverse'   a = [a, reverse a]
-          rotate'    a = [a, map reverse a]
+printSpace :: Space Char n -> IO ()
+printSpace = undefined
 
-positions :: Puzzle -> Piece -> PieceLocation
-positions z@(d:ds) p@(s:ss) = ([(x,y) | x <- [0..r], y <- [0..c]], p)
-    where r = length z - length p 
-          c = length d - length s
+solve :: (Char -> Char -> Maybe Char) -> Puzzle n -> [Board n]
+solve = undefined
 
--------------------------------------------------------------------------------
---                             Puzzle Definition                             --
--------------------------------------------------------------------------------
-
-myPieces :: [Piece]
-myPieces = [orange, cyan, yellow, purple, brown, green, red, blue]
-myStrings = ["orange", "cyan", "yellow", "purple", "brown", "green", "red", "blue"]
-
-emptyPuzzle :: Puzzle
-emptyPuzzle = ["________",
-               "________",
-               "________",
-               "________",
-               "________",
-               "________",
-               "________",
-               "________"]
-
-testPieces = [orange, cyan, yellow, purple, brown, green, red]
-testStrings = ["orange", "cyan", "yellow", "purple", "brown", "green", "red"]
-testPuzzle  = ["bbb_____",
-               "bbb_____",
-               "__b_____",
-               "__b_____",
-               "________",
-               "________",
-               "________",
-               "________"]
+rules :: Char -> Char -> Maybe Char
+rules '_'  b  = Just b
+rules  a  '_' = Just a
+rules  _   _  = Nothing
 
 
-green :: Piece
-green = ["g__g",
-         "g__g",
-         "gggg"]
+knead :: (a -> a -> Maybe a) -> Space a b -> Space a b -> Maybe (Space a b)
+knead = undefined
+{-knead f (Atom a) (Atom b) = f a b-}
+{-knead _ _ zs     []     = Just zs-}
+{-knead _ _ []     [x]    = Nothing-}
+{-knead f 0 (a:as) (b:bs) = (:) <$> f a b  <*> knead f 0     as bs-}
+{-knead f x (a:as) bs     = (:) <$> Just a <*> knead f (x-1) as bs-}
 
-cyan :: Piece
-cyan = ["_cc_",
-        "_cc_",
-        "cccc"]
-
-yellow :: Piece
-yellow = ["yy__",
-          "yyyy",
-          "yy__"]
-        
-brown :: Piece
-brown = ["__nn",
-         "nnnn",
-         "nn__"]
-
-red :: Piece
-red = ["__rrr",
-       "rrrrr"]
-
-purple :: Piece
-purple = ["pp_",
-          "ppp",
-          "ppp"]
-
-blue :: Piece
-blue = ["bb__",
-        "bb__",
-        "bbbb"]
-
-orange :: Piece
-orange = ["aaaa",
-          "aaaa"]
-
-printMatrix :: [Row] -> IO ()
-printMatrix m = putStr (unlines m)
-
-printMatrices :: [[Row]] -> IO ()
-printMatrices m = putStr $ unlines $ map unlines m
+depth :: Space a b -> Int
+depth (Atom _) = 0
+depth (Layer (a:_)) = 1 + depth a
