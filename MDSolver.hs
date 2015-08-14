16aee6b47ebe3b142dcda0124de72e5c447287bf
Jeffrey Lyman <jlyman@macalester.edu>
2015-08-14 08:13:38 +0300

    typecheck

--- MDSolver.hs
+++ MDSolver.hs
@@ -7,75 +7,60 @@
 -- definition of the peices and space share di-
 ------------------------------------------------------------------------
 
-module MDSolver () where
+module MDSolver (main) where
 
-import Data.List.Split
-import Data.List
+import Data.List hiding (map)
+import qualified Data.Map as M
+import Control.Monad
 
 main = do 
     puzzle <- readPuzzle parser3D "board.txt" pieces
     maybe (putStrLn "Error Reading puzzle files")
           (\(_,pieces) -> mapM_ (putStr . (pretty parser3D)) pieces )
-          {-((mapM_ printShape) . solve rules)-}
+          {-((mapM_ printSpace) . solve rules)-}
           puzzle
 
-parser3D = "\n\n" :- ("\n" :- Nil)
+parser3D = ["\n\n", "\n"]
 pieces = [ "piece" ++ (show x) ++ ".txt" | x <- [0..5]]
 
-type Shape n = Space n Char  
-type Board n = Shape n
-type Piece n = Shape n
-type Puzzle n = (Board n, [Piece n])
-type Parser n = Vector n String
+type Board a = Space a
+type Piece a = Space a
+type Puzzle a = (Board a, [Piece a])
+type Parser = [String]
+type Coordinate = [Int]
 
-
-data Nat = Z | S Nat
-
-infixl 6 :+
-
-type family (n :: Nat) :+ (m :: Nat) :: Nat
-type instance Z        :+ m = m
-type instance (S n)    :+ m = S (n :+ m)
-
-data Vector n a where
-    Nil  :: Vector Z a
-    (:-) :: a -> Vector n a -> Vector (S n) a
-
-instance Foldable (Vector n) where
-    foldMap f Nil    = mempty
-    foldMap f (a:-b) = (f a) `mappend` foldMap f b
-
-instance (Show a) => Show (Space n a) where
+instance (Show a) => Show (Space a) where
     show (Atom a)   = show a
     show (Layer as) = concatMap show as
 
-data Space n a where
-    Atom  :: a -> Space Z a
-    Layer :: [Space n a] -> Space (S n) a
+data Space a where
+    Atom  :: a -> Space a
+    Layer :: M.Map Int (Space a) -> Space a
 
-instance Functor (Space n) where
+instance Functor Space where
     fmap f (Atom a)   = Atom (f a)
-    fmap f (Layer ss) = Layer $ map (fmap f) ss
+    fmap f (Layer m) = Layer $ fmap (fmap f) m
 
-pretty :: (Show a) => Parser n -> Space (S n) a -> String
-pretty Nil       (Layer as) = concatMap show as 
-pretty (p :- ps) (Layer as) = concat $ (intersperse p) $ map (pretty ps) as
+pretty :: (Show a) => [String] -> Space a -> String
+pretty []     (Layer as) = concatMap show as 
+pretty (p:ps) (Layer as) = concat $ intersperse p $ children as
+    where children = map (pretty ps) . M.elems
 
-readPuzzle :: Parser n -> FilePath -> [FilePath] -> IO (Maybe (Puzzle (S n)))
+readPuzzle :: Parser -> FilePath -> [FilePath] -> IO (Maybe (Puzzle Char))
 readPuzzle p b ps = do
     bd  <- readFile b
     pcs <- mapM readFile ps
-    do
+    return $ do
         board  <- parse p bd
         pieces <- mapM (parse p) ps
-        (board, pieces)
-        
+        return (board, pieces)
 
-parse :: Parser n -> String -> Maybe (Shape (S n))
-parse Nil       = Layer . (map Atom)
-parse (p :- ps) = Layer . (map (parse ps)) . (splitOn p)
+parse :: Parser -> String -> Maybe (Space a)
+parse = undefined
+{-parse Nil       = Layer . (map Atom)-}
+{-parse (p :- ps) = Layer . (map (parse ps)) . (splitOn p)-}
 
-printSpace :: Space n Char -> IO ()
+printSpace :: Space Char -> IO ()
 printSpace = undefined
 
 solve :: (Char -> Char -> Maybe Char) -> Puzzle n -> [Board n]
@@ -86,7 +71,7 @@ rules '_'  b  = Just b
 rules  a  '_' = Just a
 rules  _   _  = Nothing
 
-knead :: (a -> a -> Maybe a) -> Space n a -> Space n a -> Maybe (Space n a)
+knead :: (a -> a -> Maybe a) -> Space a -> Space a -> Maybe (Space a)
 knead = undefined
 {-knead f (Atom a) (Atom b) = f a b-}
 {-knead _ _ zs     []     = Just zs-}
@@ -94,6 +79,35 @@ knead = undefined
 {-knead f 0 (a:as) (b:bs) = (:) <$> f a b  <*> knead f 0     as bs-}
 {-knead f x (a:as) bs     = (:) <$> Just a <*> knead f (x-1) as bs-}
 
-depth :: Space a b -> Int
-depth (Atom _) = 0
-depth (Layer (a:_)) = 1 + depth a
+-- attempt to add a layer to the nth dimension of the Space
+layer :: Int -> Space a -> Space a -> Maybe (Space a)
+layer = undefined
+
+getAtom :: Coordinate -> Space a -> Maybe a
+getAtom c s = getChunk c c s >>= get
+
+-- pulls a chunk out of the space bound by the two coordinates
+-- Coordinates must be the same length, 0 width dimensions are omitted
+getChunk :: Coordinate -> Coordinate -> Space a -> Maybe (Space a)
+getChunk = undefined
+
+getAtomWith :: (a -> Maybe a) -> Coordinate -> Space a -> Maybe a
+getAtomWith f (x:xs) (Layer m) = M.lookup x m >>= getAtomWith f xs
+getAtomWith f []     (Atom a)  = f a
+getAtomWith _ _      _         = Nothing
+    
+setAtomWith :: (a -> Maybe a) -> Coordinate -> Space a -> Maybe (Space a)
+setAtomWith f (x:xs) (Layer m) = 
+    M.lookup x m >>= setAtomWith f xs >>= \s -> Just $ Layer $ M.insert x s m
+setAtomWith f []     (Atom a)  = fmap Atom $ f a
+setAtomWith _ _      _         = Nothing
+
+atom' a@(Atom _) = Just a
+atom' _          = Nothing
+
+get :: Space a -> Maybe a
+get (Atom a) = Just a
+get _        = Nothing
+
+layer' l@(Layer _) = Just l
+atom _             = Nothing
