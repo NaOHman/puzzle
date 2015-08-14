2f1b98191eba7a87a3a4ca34b8693152c6ace82c
Jeffrey Lyman <jeffrey.lyman@appfirst.com>
2015-08-14 15:22:50 -0500

    Parsing and Printing

--- MDSolver.hs
+++ MDSolver.hs
@@ -7,82 +7,37 @@
 -- definition of the peices and space share di-
 ------------------------------------------------------------------------
 
-module MDSolver (main) where
+module Main (main) where
 
-<<<<<<< HEAD
 import Data.List hiding (map)
 import qualified Data.Map as M
+import Space
 import Control.Monad
-=======
-import Data.List.Split
-import Data.List
-import Dependent
-import Control.Applicative
->>>>>>> e14392bae01be89255b74ca70193aaee404951aa
-
-main = do 
-    puzzle <- readPuzzle parser3D "board.txt" pieces
-    maybe (putStrLn "Error Reading puzzle files")
-          (\(_,pieces) -> mapM_ (putStr . (pretty parser3D)) pieces )
-          {-((mapM_ printSpace) . solve rules)-}
-          puzzle
-
-parser3D = ["\n\n", "\n"]
-pieces = [ "piece" ++ (show x) ++ ".txt" | x <- [0..5]]
+import Text.ParserCombinators.Parsec
 
 type Board a = Space a
 type Piece a = Space a
 type Puzzle a = (Board a, [Piece a])
-type Parser = [String]
-type Coordinate = [Int]
 
-<<<<<<< HEAD
-instance (Show a) => Show (Space a) where
-    show (Atom a)   = show a
-    show (Layer as) = concatMap show as
+main = do 
+    puzzle <- readPuzzle (parseSpace delims) "board.txt" pieces
+    either (putStrLn . show)
+          (\(_,pieces) -> mapM_ (putStrLn . (pretty delims)) pieces )
+          puzzle
+    where
+        delims = ["\n\n", "\n"]
+        pieces = [ "piece" ++ (show x) ++ ".txt" | x <- [0..5]]
 
-data Space a where
-    Atom  :: a -> Space a
-    Layer :: M.Map Int (Space a) -> Space a
+          {-((mapM_ printSpace) . solve rules)-}
 
-instance Functor Space where
-    fmap f (Atom a)   = Atom (f a)
-    fmap f (Layer m) = Layer $ fmap (fmap f) m
-
-pretty :: (Show a) => [String] -> Space a -> String
-pretty []     (Layer as) = concatMap show as 
-pretty (p:ps) (Layer as) = concat $ intersperse p $ children as
-    where children = map (pretty ps) . M.elems
-=======
-pretty :: (Show a) => Parser n -> Space (S n) a -> String
-pretty Nil       (Layer as) = concatMap show as 
-pretty (p :- ps) (Layer as) = concat $ (intersperse p) $ map (pretty ps) as
->>>>>>> e14392bae01be89255b74ca70193aaee404951aa
-
-readPuzzle :: Parser -> FilePath -> [FilePath] -> IO (Maybe (Puzzle Char))
+readPuzzle :: GenParser Char () (Space a) -> FilePath -> [FilePath] -> IO (Either ParseError (Puzzle a))
 readPuzzle p b ps = do
     bd  <- readFile b
     pcs <- mapM readFile ps
     return $ do
-        board  <- parse p bd
-        pieces <- mapM (parse p) ps
+        board  <- parse p b bd
+        pieces <- zipWithM (parse p) ps pcs
         return (board, pieces)
-<<<<<<< HEAD
-
-parse :: Parser -> String -> Maybe (Space a)
-parse = undefined
-{-parse Nil       = Layer . (map Atom)-}
-{-parse (p :- ps) = Layer . (map (parse ps)) . (splitOn p)-}
-=======
-        
-
-parse :: Parser n -> String -> Maybe (Shape (S n))
-parse Nil       s = Just . Layer $ map Atom s
-parse (p :- ps) s = Just . Layer <$> map (parse ps) (splitOn p s)
->>>>>>> e14392bae01be89255b74ca70193aaee404951aa
-
-printSpace :: Space Char -> IO ()
-printSpace = undefined
 
 solve :: (Char -> Char -> Maybe Char) -> Puzzle n -> [Board n]
 solve = undefined
@@ -99,39 +54,9 @@ knead = undefined
 {-knead _ _ []     [x]    = Nothing-}
 {-knead f 0 (a:as) (b:bs) = (:) <$> f a b  <*> knead f 0     as bs-}
 {-knead f x (a:as) bs     = (:) <$> Just a <*> knead f (x-1) as bs-}
-<<<<<<< HEAD
 
--- attempt to add a layer to the nth dimension of the Space
-layer :: Int -> Space a -> Space a -> Maybe (Space a)
-layer = undefined
-
-getAtom :: Coordinate -> Space a -> Maybe a
-getAtom c s = getChunk c c s >>= get
-
--- pulls a chunk out of the space bound by the two coordinates
--- Coordinates must be the same length, 0 width dimensions are omitted
-getChunk :: Coordinate -> Coordinate -> Space a -> Maybe (Space a)
-getChunk = undefined
-
-getAtomWith :: (a -> Maybe a) -> Coordinate -> Space a -> Maybe a
-getAtomWith f (x:xs) (Layer m) = M.lookup x m >>= getAtomWith f xs
-getAtomWith f []     (Atom a)  = f a
-getAtomWith _ _      _         = Nothing
-    
-setAtomWith :: (a -> Maybe a) -> Coordinate -> Space a -> Maybe (Space a)
-setAtomWith f (x:xs) (Layer m) = 
-    M.lookup x m >>= setAtomWith f xs >>= \s -> Just $ Layer $ M.insert x s m
-setAtomWith f []     (Atom a)  = fmap Atom $ f a
-setAtomWith _ _      _         = Nothing
-
-atom' a@(Atom _) = Just a
-atom' _          = Nothing
-
-get :: Space a -> Maybe a
-get (Atom a) = Just a
-get _        = Nothing
-
-layer' l@(Layer _) = Just l
-atom _             = Nothing
-=======
->>>>>>> e14392bae01be89255b74ca70193aaee404951aa
+parseSpace :: [String] -> GenParser Char st (Space Char)
+parseSpace (x:xs) = sepBy (parseSpace xs) (string x) >>= return . fromList
+parseSpace []     = do
+    cs <- many anyChar
+    return $ fromList $ map Atom cs
---
+++ Space.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE DataKinds, GADTs, TypeFamilies, ExistentialQuantification, TypeOperators #-}
+module Space 
+    ( Space(..)
+    , Coordinate
+    , fromList
+    , get
+    , getAtom
+    , modifyAtom
+    , pretty
+    , setAtom
+    ) where
+
+import Data.List hiding (map)
+import qualified Data.Map as M
+import Control.Monad
+
+type Coordinate = [Int]
+
+data Space a where
+    Atom  :: a -> Space a
+    Layer :: M.Map Int (Space a) -> Space a
+
+instance (Show a) => Show (Space a) where
+    show (Atom a)   = show a
+    show (Layer as) = concatMap show as
+
+instance Functor Space where
+    fmap f (Atom a)   = Atom (f a)
+    fmap f (Layer m) = Layer $ fmap (fmap f) m
+
+pretty :: (Show a) => [String] -> Space a -> String
+pretty []     (Layer as) = reEscape $ concatMap show as 
+pretty (p:ps) (Layer as) = concat $ intersperse p $ children as
+    where children = map (pretty ps) . M.elems
+
+reEscape :: String -> String
+reEscape ('\\':'n':ss) = '\n' : reEscape ss
+reEscape ('\'':ss)     = reEscape ss
+reEscape (s:ss)        = s : reEscape ss
+reEscape _             = ""
+
+knead :: (a -> a -> Maybe a) -> Space a -> Space a -> Maybe (Space a)
+knead = undefined
+{-knead f (Atom a) (Atom b) = f a b-}
+{-knead _ _ zs     []     = Just zs-}
+{-knead _ _ []     [x]    = Nothing-}
+{-knead f 0 (a:as) (b:bs) = (:) <$> f a b  <*> knead f 0     as bs-}
+{-knead f x (a:as) bs     = (:) <$> Just a <*> knead f (x-1) as bs-}
+
+-- attempt to add a layer to the nth dimension of the Space
+layer :: Int -> Space a -> Space a -> Maybe (Space a)
+layer = undefined
+
+getAtom :: Coordinate -> Space a -> Maybe a
+getAtom c s = getChunk c c s >>= get
+
+-- pulls a chunk out of the space bound by the two coordinates
+-- Coordinates must be the same length, 0 width dimensions are omitted
+getChunk :: Coordinate -> Coordinate -> Space a -> Maybe (Space a)
+getChunk = undefined
+
+modifyAtom :: (a -> Maybe a) -> Coordinate -> Space a -> Maybe (Space a)
+modifyAtom f (x:xs) (Layer m) = 
+    M.lookup x m >>= modifyAtom f xs >>= \s -> Just $ Layer $ M.insert x s m
+modifyAtom f []     (Atom a)  = fmap Atom $ f a
+modifyAtom _ _      _         = Nothing
+
+setAtom :: a -> Coordinate -> Space a -> Maybe (Space a)
+setAtom = modifyAtom . (const . Just)
+
+atom' a@(Atom _) = Just a
+atom' _          = Nothing
+
+layer' l@(Layer _) = Just l
+layer' _             = Nothing
+
+get :: Space a -> Maybe a
+get (Atom a) = Just a
+get _        = Nothing
+
+fromList :: [Space a] -> Space a
+fromList ss = Layer $ snd $ foldl p (0, M.empty) ss
+    where p (x, m) s = (x+1, M.insert x s m)
