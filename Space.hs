12a11b32abea8df862c603c882692ca88b57aa52
Jeffrey Lyman <jeffrey.lyman@appfirst.com>
2015-08-20 16:02:07 -0500

    Implemented Data.Map style functions

--- Space.hs
+++ Space.hs
@@ -1,17 +1,25 @@
 {-# LANGUAGE DataKinds, GADTs, TypeFamilies, ExistentialQuantification, TypeOperators #-}
-module Space 
-    ( Space(..)
+module Shape 
+    ( Shape(..)
     , Coordinate
+    , adjust
+    , bounds
+    , empty
     , fromList
-    , get
-    {-, modifyAtom-}
+    , insert
+    , lookup
     , pretty
-    {-, setAtom-}
+    , promote
+    , singleton
+    , subspace
+    , transform
+    , union
+    , unionWith
     ) where
 
-import Data.List hiding (map, insert, lookup)
-import Prelude hiding (lookup)
 import qualified Data.IntMap.Strict as M
+import qualified Data.List as L 
+import Prelude hiding (lookup, span)
 import Control.Monad
 import Data.Maybe
 
@@ -19,82 +27,99 @@ import Data.Maybe
 
 type Coordinate = [Int]
 
-data Space a = Atom a | Layer (M.IntMap (Space a))
+data Shape a = Atom a | Layer (M.IntMap (Shape a))
 
-instance (Show a) => Show (Space a) where
+instance (Show a) => Show (Shape a) where
     show (Atom a)      = show a
     show (Layer m) = concatMap show m
 
-instance Functor Space where
+instance Functor Shape where
     fmap f (Atom a)  = Atom $ f a
     fmap f (Layer m) = Layer $ fmap (fmap f) m
 
-{-normal :: Space a -> Bool-}
-{-normal (Node m) = all atom' ss || (all layer' ss && all normal ss)-}
-    {-where ss = M.elems m-}
-{-normal (Atom  a) = True-}
-
--- knead == unionWith
-{-knead :: (a -> a -> Maybe a) -> Space a -> Space a -> Maybe (Space a)-}
-{-knead = undefined-}
-{-knead f (Atom a) (Atom b) = f a b-}
-{-knead _ _ zs     []     = Just zs-}
-{-knead _ _ []     [x]    = Nothing-}
-{-knead f 0 (a:as) (b:bs) = (:) <$> f a b  <*> knead f 0     as bs-}
-{-knead f x (a:as) bs     = (:) <$> Just a <*> knead f (x-1) as bs-}
-
--- pulls a chunk out of the space bound by the two coordinates
--- Coordinates must be the same length, 0 width dimensions are omitted
-{-getChunk :: Coordinate -> Coordinate -> Space a -> Maybe (Space a)-}
-{-getChunk = undefined-}
-
-
-lookup :: Coordinate -> Space a -> Maybe a
+-- Gets the value at the given coordinate
+lookup :: Coordinate -> Shape a -> Maybe a
 lookup []     (Atom a)  = Just a
 lookup (c:cs) (Layer l) = M.lookup c l >>= lookup cs
 lookup _      _         = Nothing
 
-adjust :: (a -> a) -> Coordinate -> Space a -> Space a
+-- modifies the falue at the give coordinate if it exists
+adjust :: (a -> a) -> Coordinate -> Shape a -> Shape a
 adjust f []     (Atom a) = Atom $ f a
+adjust _ []     s        = s
 adjust f (c:cs) s        = withLayer s $ M.adjust (adjust f cs) c
 
-insert :: Coordinate -> a -> Space a -> Space a
-insert []     a _          = Atom a
-insert cs     a s@(Atom _) = insert cs a $ promote s
-insert (c:cs) a l = withLayer l $ M.insert c sublayer
-    where sublayer = insert cs a $ fromMaybe emptyLayer (subspace c l)
+transform :: (a -> M.IntMap (Shape b) -> M.IntMap (Shape b)) -> [a] -> Shape b -> Shape b
+transform _ [] s = s
+transform f (c:cs) l = withLayer l $ f c . fmap (transform f cs)
 
-promote :: Space a -> Space a
+unionWith :: (Shape a -> Shape a -> Shape a) -> Shape a -> Shape a -> Shape a
+unionWith f (Layer a) (Layer b) = Layer $ M.unionWith f a b
+unionWith f a         b         = f a b
+
+union :: Shape a -> Shape a ->  Shape a
+union = unionWith const
+
+insert :: Coordinate -> a -> Shape a -> Shape a 
+insert c a = union (singleton c a)
+
+empty = Layer M.empty
+
+singleton :: Coordinate -> a -> Shape a
+singleton []     = Atom
+singleton (c:cs) = Layer . M.singleton c . singleton cs
+
+promote :: Shape a -> Shape a
 promote a@(Atom _) = Layer $ M.singleton 0 a
 promote l          = l
 
-withLayer :: Space a -> (M.IntMap (Space a) -> M.IntMap (Space a)) -> Space a
+withLayer :: Shape a -> (M.IntMap (Shape a) -> M.IntMap (Shape a)) -> Shape a
 withLayer (Layer m) f = Layer $ f m
 withLayer s _         = s
+
+shift :: Coordinate -> Shape a -> Shape a
+shift = transform (\a -> M.mapKeysMonotonic (a+))
+
+subspace :: M.Key -> Shape a -> Maybe (Shape a)
+subspace c (Layer m) = M.lookup c m
+subspace _ _         = Nothing
 
 isAtom (Atom _) = True
 isAtom _        = False
 
 isLayer = not . isAtom
 
-get :: Space a -> Maybe a
+get :: Shape a -> Maybe a
 get (Atom a) = Just a
 get _        = Nothing
 
-subspace :: M.Key -> Space a -> Maybe (Space a)
-subspace c (Layer m) = M.lookup c m
-subspace _ _         = Nothing
-
-emptyLayer = Layer M.empty
-
-fromList :: [a] -> Space a
+fromList :: [a] -> Shape a
 fromList = Layer . M.fromList . zipWith (\k v -> (k, Atom v)) [0..]
 
+span :: Ord a => [a] -> (a,a)
+span (x:xs) = L.foldl' (\a b -> wider a (b,b)) (x,x) xs
+
+reduce :: ([a] -> a) -> [[a]] -> [a] 
+reduce _ ([]:_) = []
+reduce f xs     = f (map head xs) : reduce f (map tail xs)
+    
+wider :: Ord a => (a,a) -> (a,a) -> (a,a)
+wider (a,b) (c,d) = (min a c, max b d)
+
+widest :: Ord a => [(a,a)] -> (a,a)
+widest (x:xs) = L.foldl' wider x xs
+
+bounds :: Shape a -> [(M.Key, M.Key)]
+bounds (Atom a)  = []
+bounds (Layer l) = let b  = span $ M.keys l
+                       bs = reduce widest $ M.foldr' ((:) . bounds) [] l
+                   in b:bs
 -- Printing
-pretty :: (Show a) => [String] -> Space a -> String
-pretty _      (Atom a)   = show a
-pretty []     (Layer as) = reEscape $ concatMap show as 
-pretty (p:ps) (Layer as) = intercalate p $ map (pretty ps) (M.elems as)
+pretty :: (Show a) => String -> [String] -> Shape a -> String
+pretty e ds s = pretty' (bounds s) e ds s
+pretty' _ _ _ (Atom a)   = show a
+pretty' ((l,hi):bs) e (p:ps) (Layer as) = undefined
+    {-L.intercalate p $ map (pretty e ps) (M.elems as)-}
 
 reEscape :: String -> String
 reEscape ('\\':'n':ss) = '\n' : reEscape ss
